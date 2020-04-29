1d1e06c987900321bba636f6ed3d640de84a0373
齐文豪 <dawangong@126.com>
2020-04-29 16:18:11 +0800

    prefetch/preload

--- src/app/a.js
+++ src/app/a.js
@@ -4,7 +4,10 @@ import './a.less'
 import data from './data.json'
 import _ from 'lodash';
 
-// import (/* webpackChunkName:"lodash" */ 'lodash');
+/* 懒加载 默认触发加载 */ // 最简
+// 更改打包后的name import (/* webpackChunkName:"lodash" */ 'lodash');
+// 网络空闲加载（进行缓存） import (/* webpackPrefetch:"lodash" */ 'lodash'); // 最佳
+// 和主文件一起加载（进行缓存） import (/* webpackPreload:"lodash" */ 'lodash');// 不推荐
 
 create('img', 'img', test);
 
