96b330a150922d91177317f7585e62c443570ef0
齐文豪 <dawangong@126.com>
2020-04-29 16:18:46 +0800

    lazy load

--- src/app/a.js
+++ src/app/a.js
@@ -4,7 +4,7 @@ import './a.less'
 import data from './data.json'
 import _ from 'lodash';
 
-/* 懒加载 默认触发加载 */ // 最简
+/* 懒加载 默认触发加载 */ // 最简(牺牲体验)
 // 更改打包后的name import (/* webpackChunkName:"lodash" */ 'lodash');
 // 网络空闲加载（进行缓存） import (/* webpackPrefetch:"lodash" */ 'lodash'); // 最佳
 // 和主文件一起加载（进行缓存） import (/* webpackPreload:"lodash" */ 'lodash');// 不推荐
