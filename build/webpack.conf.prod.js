d0ee1d9de24e85d17b33123a3e2fdf60ef552bb4
齐文豪 <dawangong@126.com>
2020-04-29 16:14:22 +0800

    min?

--- build/webpack.conf.prod.js
+++ build/webpack.conf.prod.js
@@ -1,10 +1,16 @@
 const merge = require('webpack-merge');
 const commonConfig = require('./webpack.conf.common');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+// const UglifyJsPlugin = require('uglifyjs-webpack-plugin'); ？反而会变得更大？
 
 const prodConfig = {
   plugins: [
-    new BundleAnalyzerPlugin()
+    new BundleAnalyzerPlugin(),
+    // new UglifyJsPlugin({
+    //   cache: true,
+    //   parallel: true,
+    //   sourceMap: true
+    // })
   ]
 };
 
