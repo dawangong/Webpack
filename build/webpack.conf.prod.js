f0a33b632cc859f9cc4ecb5d21d10dd75a2f8c2d
齐文豪 <dawangong@126.com>
2020-04-29 16:07:32 +0800

    Analyzer

--- build/webpack.conf.prod.js
+++ build/webpack.conf.prod.js
@@ -1,8 +1,11 @@
 const merge = require('webpack-merge');
 const commonConfig = require('./webpack.conf.common');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 const prodConfig = {
-  plugins: []
+  plugins: [
+    new BundleAnalyzerPlugin()
+  ]
 };
 
 module.exports = merge(prodConfig, commonConfig);
